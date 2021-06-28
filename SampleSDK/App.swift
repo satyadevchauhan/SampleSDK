0efcb9706c0564f09f9e9689e0887a35f75c81cf
Satyadev Chauhan <satyadevchauhan@gmail.com>
2021-06-28 13:14:13 +0530

    Add featureOne to App.

---
+++ SampleSDK/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  SampleSDK
+//
+//  Created by Chauhan, Satyadev on 28/06/21.
+//
+
+import Foundation
+
+public class App {
+    
+    public init() {
+        // TODO
+    }
+    
+    public func featureOne() {
+        print("FeatureOne")
+    }
+}
