e5d01b93264adf4ee1dfca93fbb588e2679b5e18
Satyadev Chauhan <satyadevchauhan@gmail.com>
2024-08-11 12:10:39 +0530

    Remove unused file.

--- build-xcframework.sh
+++ build-xcframework.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -o pipefail
 
+# Remove build folder, if already exists
+rm -rf ./build/
+
 # Clean Archive iPhone SampleSDK
 xcodebuild clean archive \
     -scheme SampleSDK \
@@ -21,7 +24,14 @@ xcodebuild clean archive \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
+# Create xcframework from Archives
 xcodebuild -create-xcframework \
     -framework './build/SampleSDK-iphonesimulator.xcarchive/Products/Library/Frameworks/SampleSDK.framework' \
     -framework './build/SampleSDK-iphoneos.xcarchive/Products/Library/Frameworks/SampleSDK.framework' \
     -output './build/SampleSDK.xcframework'
+    
+# Copy *.xcframework to Framework
+mkdir -p ./Framework && cp -R ./build/SampleSDK.xcframework ./Framework/SampleSDK.xcframework
+
+echo "SampleSDK.xcframework completed successfully."
+open Framework/
